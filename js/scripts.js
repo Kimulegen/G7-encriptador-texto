bd3a4a49c578dd0816609ee481313acdeff2ccb4
kimulegen <kimulegen@proton.me>
2024-08-09 12:30:43 -0400

    feat: remove placeholder input text

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log('connected!')
+const $ = (el) => document.querySelector(el)
+
+const input = $('.input-text')
+
+let firstClick = false
+input.addEventListener('click', (e) => {
+    e.target.textContent = ''
+    console.log('erased')
+}, {once :true})
